a61aeaed5d02c27736a213e301a0b7496312c490
Felix <wpaphk@gmail.com>
2022-11-04 08:11:31 +0000

    .env sample file

--- backend/database/seeders/SiteSettingSeeder.php
+++ backend/database/seeders/SiteSettingSeeder.php
@@ -17,7 +17,7 @@ class SiteSettingSeeder extends Seeder
     public function run()
     {
         DB::table('site_settings')->insert([
-            'background_color' => 'linear-gradient(144deg, rgb(41, 47, 60) 0%, rgb(41, 72, 107) 46%, rgb(20, 19, 57) 100%)',
+            'background_color' => 'linear-gradient(to left, #4155b3 50%, #c34875 50%)',
             'btn_color' => 'ff84b7',
             'live' => 3,
             'card_face' => '',
